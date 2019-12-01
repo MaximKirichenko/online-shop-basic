5c741d33507b5785567d1a15ed035fcc9742682f
Kirichenko Aleksandra <mkyrychenko@luxoft.com>
2019-12-01 17:46:22 +0200

    Replace all servlets to Controller methods

--- src/main/java/com/study/onlineshop/controller/OnlineShopController.java
+++ src/main/java/com/study/onlineshop/controller/OnlineShopController.java
@@ -25,7 +25,7 @@ public class OnlineShopController {
     private ProductService productService = (ProductService) ServiceLocator.getService("productService");
 
     @RequestMapping(method = RequestMethod.GET)
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void listAllProducts(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PageGenerator pageGenerator = PageGenerator.instance();
         List<Product> products = productService.getAll();
 
@@ -34,6 +34,70 @@ public class OnlineShopController {
 
         String page = pageGenerator.getPage("products", parameters);
         response.getWriter().write(page);
+    }
+
+    @RequestMapping(path = "/product/edit/", method = RequestMethod.GET)
+    public void editPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int id = getId(request.getRequestURI());
+        PageGenerator pageGenerator = PageGenerator.instance();
+        Product product = productService.getById(id);
+
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("product", product);
+
+        String page = pageGenerator.getPage("edit", parameters);
+        response.getWriter().write(page);
+    }
+
+    @RequestMapping(path = "/product/edit/", method = RequestMethod.POST)
+    public void editEntry(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        double price = Double.valueOf(request.getParameter("price"));
+        if(name == null || name.isEmpty() || price <= 0d){
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            int id = getId(request.getRequestURI());
+            productService.update(id, name, price);
+            response.sendRedirect("/products");
+        }
+    }
+
+    @RequestMapping(path = "/products/add", method = RequestMethod.GET)
+    protected void addProductPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PageGenerator pageGenerator = PageGenerator.instance();
+
+        HashMap<String, Object> parameters = new HashMap<>();
+
+        String page = pageGenerator.getPage("add", parameters);
+        response.getWriter().write(page);
+    }
+
+    @RequestMapping(path = "/products/add", method = RequestMethod.POST)
+    protected void addProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        double price = Double.valueOf(request.getParameter("price"));
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        int id = productService.add(product);
+        product.setId(id);
+        response.sendRedirect("/products");
+    }
+
+    @RequestMapping(path = "/product/delete/", method = RequestMethod.POST)
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        int index = uri.lastIndexOf("/");
+        int id = Integer.valueOf(uri.substring(index+1, uri.length()));
+        productService.delete(id);
+        resp.sendRedirect("/products");
+    }
+
+    private int getId(String uri){
+        int index = uri.lastIndexOf("/");
+        int id = Integer.valueOf(uri.substring(index+1, uri.length()));
+        return id;
     }
 
     public void setProductService(ProductService productService) {
